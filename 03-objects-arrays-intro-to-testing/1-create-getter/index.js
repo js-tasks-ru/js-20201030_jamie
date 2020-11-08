17081bc18fb8e87c81fe47424d759a5d723ed46c
Jamie <tabby7115@gmail.com>
2020-11-08 22:00:58 +0700

    Add createGetter task from the third task

--- 03-objects-arrays-intro-to-testing/1-create-getter/index.js
+++ 03-objects-arrays-intro-to-testing/1-create-getter/index.js
@@ -4,5 +4,15 @@
  * @returns {function} - function-getter which allow get value from object by set path
  */
 export function createGetter(path) {
+    const objProperties = path.split('.');
 
+    return (obj) => {
+        let value = obj[objProperties[0]];
+
+        for (let i = 1; i < objProperties.length; i++){
+            if (value === undefined) return value;
+            value = value[objProperties[i]];
+        }
+        return value;
+    }
 }
